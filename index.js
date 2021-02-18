84d237356b82bc2edd452d1bad11fb7e9acae95a
Nemanja Bulatovic <bulatovic@work.co>
2021-02-18 17:29:08 +0100

    gallery in progress

--- index.js
+++ index.js
@@ -52,15 +52,6 @@ const searchSlide = () => {
     });
 }
 
-document.getElementById("body").onscroll =  myFunction = () => {  
-    var scrolltotop = document.scrollingElement.scrollTop;
-    var target = document.getElementById("main1");
-    var xvalue = "center";
-    var factor = 0.5;
-    var yvalue = scrolltotop * factor;
-    target.style.backgroundPosition = xvalue + " " + yvalue + "px";
-  }
-
 fixedHeader();
 navigationSlide();
 searchSlide();
