da4711a1305c99eed7c5b3c114311e1cb4f7e727
Nemanja Bulatovic <bulatovic@work.co>
2021-02-19 11:22:04 +0100

    news

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const search = document.querySelector('.search-icon');
-const form = document.querySelector('.search-form');
+// const form = document.querySelector('.search-form');
 const navigation = document.querySelector('.navigation-list');
 const burger = document.querySelector('.navigation-burger');
 const navigationLinks = document.querySelectorAll('.navigation-list-item');
 const hero = document.querySelector('.hero');
 const header = document.querySelector('.header');
+const input = document.querySelector('.search-input');
 
 const fixedHeader = () => {
     window.addEventListener('scroll', () => {
@@ -22,9 +23,9 @@ const navigationSlide = () => {
         document.getElementsByTagName('body')[0].classList.toggle('hidden')
         navigation.classList.toggle('navigation-list-active');
         burger.classList.toggle('toggle');
-        if(form.classList.contains('search-form-active')){
-            form.classList.remove('search-form-active');
-        }
+        // if(form.classList.contains('search-form-active')){
+        //     form.classList.remove('search-form-active');
+        // }
         navigationLinks.forEach((link, index) => {
             if(link.style.animation){
                 link.style.animation= ``
@@ -35,23 +36,26 @@ const navigationSlide = () => {
     });
 }
 
-const searchSlide = () => {
-    search.addEventListener('click', () => {
-        form.classList.toggle('search-form-active');
-        if(navigation.classList.contains('navigation-list-active')){
-            navigation.classList.remove('navigation-list-active');
-            burger.classList.remove('toggle');
-            navigationLinks.forEach((link, index) => {
-                if(link.style.animation){
-                    link.style.animation= ``
-                }else{
-                    link.style.animation= `navigationLinkFade 0.3s forwards ${index / 7 + 0.2}s`
-                }
-            });
-        }
-    });
-}
+// const searchSlide = () => {
+//     search.addEventListener('click', () => {
+//         form.classList.toggle('search-form-active');
+//         if(navigation.classList.contains('navigation-list-active')){
+//             navigation.classList.remove('navigation-list-active');
+//             burger.classList.remove('toggle');
+//             navigationLinks.forEach((link, index) => {
+//                 if(link.style.animation){
+//                     link.style.animation= ``
+//                 }else{
+//                     link.style.animation= `navigationLinkFade 0.3s forwards ${index / 7 + 0.2}s`
+//                 }
+//             });
+//         }
+//     });
+// }
+
+
+
 
 fixedHeader();
 navigationSlide();
-searchSlide();
+// searchSlide();
--- parallax.js
+++ parallax.js
@@ -3,6 +3,6 @@ document.getElementById("body").onscroll =  myFunction = () => {
     var target = document.getElementById("main1");
     var xvalue = "center";
     var factor = 0.5;
-    var yvalue = scrolltotop * factor;
+    var yvalue =  205 - scrolltotop * factor;
     target.style.backgroundPosition = xvalue + " " + yvalue + "px";
   }
